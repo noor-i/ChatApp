a9d15830f0cbb0e1f7b983396be5c5d11e5374fc
mia16 <mahnur.imran@gmail.com>
2025-06-11 17:11:29 -0700

    Create User schema and model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,1 +1,31 @@
 import mongoose from "mongoose";
+
+// create a schema for users
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+// create a model based on the schema
+const User = mongoose.model("User", userSchema);
+
+export default User;
