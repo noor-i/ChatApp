465463730b864b086c09eca8789241a9cd3c1eed
mia16 <mahnur.imran@gmail.com>
2025-06-15 00:32:22 -0700

    Login auth controller complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -50,8 +50,37 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login route");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //Check if email exists in DB (is an existing user)
+    const user = await User.findOne({ email });
+
+    // If email does not exists in DB
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    //If email exists, check if the password is correct using bcrypt compare method
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
