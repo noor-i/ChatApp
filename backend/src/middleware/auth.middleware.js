8dde60cb35e8f944040d6926ce934dc70374bad1
mia16 <mahnur.imran@gmail.com>
2025-06-23 00:39:31 -0700

    Method protectRoute complete

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,12 +4,31 @@ import User from "../models/user.model";
 export const protectRoute = async (req, res, next) => {
   try {
     //Check if there is a token or not
-
     const token = req.cookies.jwt;
     if (!token) {
       return res
         .status(401)
         .json({ message: "Unauthorized - No Token Provided" });
     }
-  } catch (error) {}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // If decoded is a faulsy value, return error message
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
+    // If decoded value is not faulsy, we can find the user in DB
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // If the user is authenticated, add user to the request and call the next function (updateProfile) in this case
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
