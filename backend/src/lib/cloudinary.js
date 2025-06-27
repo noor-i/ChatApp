fb7f3b88dc7f0be1a12aaaeede909f54db866258
mia16 <mahnur.imran@gmail.com>
2025-06-26 23:51:45 -0700

    Set up Cloudinary and its secrets/keys

---
+++ backend/src/lib/cloudinary.js
@@ -0,0 +1,16 @@
+/* This imports the Cloudinary library, which is used for uploading and managing images and videos.*/
+// This is for the user profile picture upload feature
+
+import { v2 as cloudinary } from "cloudinary";
+
+import { config } from "dotenv";
+
+config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export default cloudinary;
