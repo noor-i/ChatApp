4cd82a1294972f587f0d45196eb3954806671204
mia16 <mahnur.imran@gmail.com>
2025-07-14 22:03:32 -0700

    Mount message routes at /api/message endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
@@ -11,10 +12,11 @@ const PORT = process.env.PORT;
 
 // Allows you to extract json data out of body
 app.use(express.json());
-//Allows you to parse the cookie to grab value of token
+// Allows you to parse the cookie to grab value of token
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT: " + PORT);
