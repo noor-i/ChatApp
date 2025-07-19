40067b167643d3feee9f5180110d5ae6de236614
mia16 <mahnur.imran@gmail.com>
2025-07-18 20:10:48 -0700

    Complete getMessages for retreiving messages between two users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -11,6 +12,27 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(filteredUsers);
   } catch (error) {
     console.log("Error in getUsersForSidebar: ", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    // We use req.params because the user ID is being passed in the URL path (e.g., /messages/:id)
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    // Send all messages between me and this user from either end
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages: ", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 // For fetching users on the sidebar
 router.get("/user", protectRoute, getUsersForSidebar);
+
+// the user id we would like to fetch our messages with
 router.get("/:id", protectRoute, getMessages);
 
 export default router;
