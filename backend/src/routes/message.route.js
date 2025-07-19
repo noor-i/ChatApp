43c22f6eb4f48ad911ee735649812cd8a4b50d22
mia16 <mahnur.imran@gmail.com>
2025-07-18 18:51:28 -0700

    Complete getUsersForSidebar method

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 // For fetching users on the sidebar
 router.get("/user", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
