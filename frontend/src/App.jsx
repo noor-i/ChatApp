73a70baec4d29f3565daae23861242558adb379d
mia16 <mahnur.imran@gmail.com>
2025-07-19 18:39:31 -0700

    Added Routes and created a Route for home,signup,login,settings,and profile pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   );
 };
