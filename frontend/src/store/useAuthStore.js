e8503fff089eaf36ba8f05f694e7c8a1ba418275
mia16 <mahnur.imran@gmail.com>
2025-07-27 21:28:08 -0700

    Created Zustand store to manage authentication state

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,25 @@
+// Zustand store for managing authentication state in the app.
+// Tracks user info, loading states, and provides a function to check authentication status with the backend.
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null, // at first we don't know if user is authenticated or not
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true, // loading state for checking if user is authenticated
+
+  checkAuth: async () => {
+    try {
+      // We already provided the base url in the axios instance baseURL field (axios.js)
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
