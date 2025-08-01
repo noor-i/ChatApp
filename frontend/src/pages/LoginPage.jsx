fbd804cbeb9ca6247063d0d222b3f2dd1b788bc1
mia16 <mahnur.imran@gmail.com>
2025-08-01 02:46:07 -0700

    Complete Login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import toast from "react-hot-toast";
-import { MessageSquare } from "lucide-react";
+import { Eye, EyeOff, Mail, MessageSquare, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+import AuthImagePattern from "../components/AuthImagePattern";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -34,7 +36,7 @@ const LoginPage = () => {
 
   return (
     <div className="h-screen grid lg:grid-cols-2">
-      {/* LEFT SIDE FORM */}
+      {/* LEFT SIDE */}
       <div className="flex flex-col items-center justify-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
           {/* LOGO AND INTRO */}
@@ -50,8 +52,90 @@ const LoginPage = () => {
               <p className="text-base-content/60">Sign in to your account</p>
             </div>
           </div>
+          {/* FORM */}
+          <form onSubmit={handleSubmit} className="space-y-6">
+            {/* EMAIL FORM CONTROL */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute z-10 inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="string"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                ></input>
+              </div>
+            </div>
+            {/* PASSWORD FORM CONTROL */}
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 z-10 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-5 w-5 text-base-content/40" />
+                </div>
+                <input
+                  placeholder="••••••••"
+                  type="string"
+                  className="input input-bordered w-full pl-10"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                ></input>
+                <button
+                  type="button"
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <Eye className="size-5 text-base-content/40" />
+                  ) : (
+                    <EyeOff className="size-5 text-base-content/40" />
+                  )}
+                </button>
+              </div>
+            </div>
+            {/* SUBMIT FORM BUTTON */}
+            <button type="submit" className="btn btn-primary w-full">
+              {isLoggingIn ? (
+                <>
+                  <Loader className="size-5 animate-spin" />
+                  Loading...
+                </>
+              ) : (
+                "Sign in"
+              )}
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base-content/60">
+              Don&apos;t have an account?{" "}
+              <Link to="/signup" className="link link-primary">
+                Create account
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
+
+      {/* RIGHT SIDE */}
+      <AuthImagePattern
+        title={"Welcome back!"}
+        subtitle={
+          "Sign in to continue your conversations and catch up with your messages."
+        }
+      />
     </div>
   );
 };
