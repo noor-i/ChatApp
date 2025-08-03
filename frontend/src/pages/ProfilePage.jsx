8c5ab7df8fb31cebda16d45010c36bf0cdcefe2b
mia16 <mahnur.imran@gmail.com>
2025-08-02 20:35:38 -0700

    Complete first half of Profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Camera } from "lucide-react";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { authUser, updateProfile, isUpdatingProfile } = useAuthStore();
+  const [profilePic, setProfilePic] = useState("");
+  const handleImageUpload = async (e) => {};
+
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto">
+        {/* FIRST HALF */}
+        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold ">Profile</h1>
+            <p className="mt-2">Your profile information</p>
+          </div>
+          {/* avatar upload */}
+          <div className="flex flex-col items-center gap-4">
+            <div className="relative">
+              <img
+                className="size-32 rounded-full object-cover border-3"
+                src={authUser.profilePic || "../public/avatar.png"}
+                alt="profile-pic"
+              />
+              <label
+                htmlFor="avatar-upload"
+                className={`absolute bottom-0 right-0 bg-base-content rounded-full p-2 cursor-pointer hover:scale-105 ${
+                  isUpdatingProfile ? "animate-pulse pointer-events-none" : ""
+                }`}
+              >
+                <Camera className="w-5 h-5 text-base-200" />
+                <input
+                  type="file"
+                  id="avatar-upload"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                  disabled={isUpdatingProfile}
+                />
+              </label>
+            </div>
+            <p className="text-sm text-zinc-400">
+              {isUpdatingProfile
+                ? "Uploading..."
+                : "Click the camera icon to update your photo"}
+            </p>
+          </div>
+          {/* User info */}
+          <div></div>
+        </div>
+        {/* SECOND HALF */}
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
