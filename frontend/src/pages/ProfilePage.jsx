79892251f5a3a50fb7a2bca34a334f55989f3da5
mia16 <mahnur.imran@gmail.com>
2025-08-02 20:54:21 -0700

    Complete UI for profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
-import { Camera } from "lucide-react";
+import { Camera, Mail, User } from "lucide-react";
 
 const ProfilePage = () => {
   const { authUser, updateProfile, isUpdatingProfile } = useAuthStore();
@@ -9,7 +9,7 @@ const ProfilePage = () => {
 
   return (
     <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto">
+      <div className="max-w-2xl mx-auto p-4 py-8">
         {/* FIRST HALF */}
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
@@ -48,10 +48,41 @@ const ProfilePage = () => {
             </p>
           </div>
           {/* User info */}
-          <div></div>
+          <div className="space-y-6">
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <User className="w-4 h-4" />
+                Full Name
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.fullName}
+              </p>
+            </div>
+            <div className="space-y-1.5">
+              <div className="text-sm text-zinc-400 flex items-center gap-2">
+                <Mail className="w-4 h-4" />
+                Email
+              </div>
+              <p className="px-4 py-2.5 bg-base-200 rounded-lg border">
+                {authUser?.email}
+              </p>
+            </div>
+          </div>
         </div>
         {/* SECOND HALF */}
-        <div></div>
+        <div className="bg-base-300 rounded-xl p-6 mt-6">
+          <h1 className="text-lg font-medium  mb-4">Account Information</h1>
+          <div className="space-y-3 text-sm">
+            <div className="flex items-center justify-between py-2 border-b border-zinc-700">
+              <span>Member Since</span>
+              <span>{authUser.createdAt?.split("T")[0]}</span>
+            </div>
+            <div className="flex items-center justify-between py-2">
+              <span>Account Status</span>
+              <span className="text-green-500">Active</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
