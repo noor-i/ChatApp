eaf8e0c6d4da2717b59be7c7f70857361d9da92f
mia16 <mahnur.imran@gmail.com>
2025-08-03 02:44:07 -0700

    Create state hook for theme using localStorage

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+  theme: () => localStorage.getItem("theme") || "cupcake",
+  setTheme: (theme) => {
+    localStorage.setItem("theme", theme); // Save theme to local storage
+    set({ theme }); // Update state
+  },
+}));
