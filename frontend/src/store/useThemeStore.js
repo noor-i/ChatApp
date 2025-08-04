26e4a03a5ceff994bcc65174a70cee269bf859a0
mia16 <mahnur.imran@gmail.com>
2025-08-04 00:07:03 -0700

    Change retrieval of theme from local storage via static variable to fix bug of state not being saved

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import React from "react";
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: () => localStorage.getItem("theme") || "cupcake",
+  theme: localStorage.getItem("theme") || "cupcake",
   setTheme: (theme) => {
     localStorage.setItem("theme", theme); // Save theme to local storage
     set({ theme }); // Update state
