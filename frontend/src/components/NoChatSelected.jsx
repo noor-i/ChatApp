731f758fe184be470a2be8c2819faa707058381b
mia16 <mahnur.imran@gmail.com>
2025-08-04 05:44:20 -0700

    Modify text alignment and width of component for responsiveness

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NoChatSelected = () => {
   return (
-    <div className="w-full flex-1 flex flex-col items-center justify-center space-y-8 p-4 bg-base-100/50">
+    <div className="w-full max-w-md flex-1 flex flex-col items-center justify-center space-y-7 p-4 bg-base-100/50 text-center">
       <div className="border-2 p-3 rounded-xl bg-primary/10 animate-bounce">
         <MessageSquare className="size-8 text-primary" />
       </div>
