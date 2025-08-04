07a0e061ddcee7f35de35ad80574b2c9aaa3958d
mia16 <mahnur.imran@gmail.com>
2025-08-04 05:25:09 -0700

    Create state managment for home page chat components

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  // To see if users or messages are showing for showing loading state
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("message/users");
+      set({ users: res.data });
+    } catch (error) {
+      console.log("Error in getting users");
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  getMessages: async () => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/message/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      console.log("Error in getting messages.");
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+
+  // optimize this later TODO
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
