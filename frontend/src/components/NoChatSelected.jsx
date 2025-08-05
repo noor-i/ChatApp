4f51c2b417ee36946c428133afceea3b6a83b9ff
mia16 <mahnur.imran@gmail.com>
2025-08-05 00:28:29 -0700

    Fix styling to recenter logo and text

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -1,16 +1,27 @@
 import { MessageSquare } from "lucide-react";
-import React from "react";
 
 const NoChatSelected = () => {
   return (
-    <div className="w-full max-w-md flex-1 flex flex-col items-center justify-center space-y-7 p-4 bg-base-100/50 text-center">
-      <div className="border-2 p-3 rounded-xl bg-primary/10 animate-bounce">
-        <MessageSquare className="size-8 text-primary" />
+    <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
+      <div className="max-w-md text-center space-y-6">
+        {/* Icon Display */}
+        <div className="flex justify-center gap-4 mb-4">
+          <div className="relative">
+            <div
+              className="w-16 h-16 rounded-2xl bg-primary/10 flex items-center
+             justify-center animate-bounce"
+            >
+              <MessageSquare className="w-8 h-8 text-primary " />
+            </div>
+          </div>
+        </div>
+
+        {/* Welcome Text */}
+        <h2 className="text-2xl font-bold">Welcome to Chatty!</h2>
+        <p className="text-base-content/60">
+          Select a conversation from the sidebar to start chatting
+        </p>
       </div>
-      <h2 className="text-2xl font-bold">Welcome to Chai-Chat!</h2>
-      <p className="text-base-content/60">
-        Select a conversation from the sidebar to start chai-chatting
-      </p>
     </div>
   );
 };
