1a2f60841a6b138ba464638946d2bd2419e162cf
mia16 <mahnur.imran@gmail.com>
2025-08-05 02:39:50 -0700

    Sidebar component complete except for online user toggle functionality

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import { Users } from "lucide-react";
+import React, { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
 
 const Sidebar = () => {
-  return <div></div>;
+  const { users, selectedUser, isUsersLoading, getUsers, setSelectedUser } =
+    useChatStore();
+
+  const { onlineUsers } = useAuthStore();
+
+  // Fetch the list of users when the Sidebar component mounts
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
+  // Show loading skeleton while users are being fetched.
+  // Early returns like this are common in React to show loading, error, or empty states before rendering the main UI.
+  if (isUsersLoading) return <SidebarSkeleton />;
+
+  return (
+    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+      <div className="w-full border-b border-base-300 p-5">
+        {/* Sidebar header */}
+        <div className="flex items-center gap-2">
+          <Users className="size-6" />
+          <span className="font-medium hidden lg:block">Contacts</span>
+        </div>
+
+        {/* TO DO : Online Filter Toggle */}
+      </div>
+
+      {/* Contact List */}
+      <div className="overflow-y-auto w-full">
+        {users.map((user) => (
+          <button
+            key={user._id}
+            onClick={() => setSelectedUser(user)}
+            className={`w-full p-3 flex items-center gap-3
+              hover:bg-base-300 transition-colors ${
+                selectedUser?._id === user._id
+                  ? "bg-base-300 ring-1 ring-base-300"
+                  : ""
+              }`}
+          >
+            <div className="relative mx-auto lg:mx-0">
+              <img
+                src={user.profilePic || "/avatar.png"}
+                alt={user.name}
+                className="size-12 object-cover rounded-full"
+              />
+              {onlineUsers.includes(user._id) && (
+                <span className="absolute bottom-0 right-0 size-3 bg-green-500 rounded-full ring-2 ring-zinc-900" />
+              )}
+            </div>
+
+            {/* User info - only visible on larger screens */}
+            <div className="hidden lg:block text-left min-w-0">
+              <div className="font-medium truncate">{user.fullName}</div>
+              <div className="text-sm text-zinc-400">
+                {onlineUsers.includes(user._id) ? "Online" : "Offline"}
+              </div>
+            </div>
+          </button>
+        ))}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
