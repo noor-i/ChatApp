7e5b66eeaf24fbf7bff3402da6c43c5549e50fc5
mia16 <mahnur.imran@gmail.com>
2025-08-18 03:08:29 -0700

    Add io.emit() to send events based on new conneced/disconnected users

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -17,14 +17,26 @@ const io = new Server(server, {
   },
 });
 
+// Use to store online users
+const userSocketMap = {}; // (key : value) => (userId : socketId)
+
 // Listen for new socket connections from clients
 io.on("connection", (socket) => {
   // This runs every time a client connects via Socket.io
   console.log("A user connected", socket.id);
 
+  // socket.handshake contains connection details sent by the client
+  const userId = socket.handshake.query.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all connected clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
   // Listen for when the client disconnects
   socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
 
