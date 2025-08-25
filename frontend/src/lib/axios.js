226751b55b5a1940423564aeaeeef8661e3dc8ca
mia16 <mahnur.imran@gmail.com>
2025-08-25 03:54:36 -0700

    Update URL to be dynamic for dev and prod environments

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 
 // Create a pre-configured axios instance with a base URL and credentials enabled
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // Base URL for all API requests
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api", // URL for all API requests
   withCredentials: true, // Send cookies with requests (for authentication)
 });
